5f90ec28f5f446769885a0a2b5fa09f45e59d666
Ximena Gallego <mxgallego@unicauca.edu.co>
2023-02-24 21:33:08 -0500

    el concepto y como declarar char y String como concatenar

---
+++ javaprimerospasos/src/EjemploValoresVariables.java
@@ -0,0 +1,16 @@
+
+public class EjemploValoresVariables {
+	
+	public static void main(String[] args) {
+		
+		int numero1 = 5;
+		int numero2 = 9;
+		System.out.println(numero2);
+		
+		numero2 = numero1; //5
+		System.out.println(numero2);
+		
+		numero1 = 88;
+		System.out.println(numero2);
+	}
+}
---
+++ javaprimerospasos/src/TipoVariable.java
@@ -0,0 +1,18 @@
+
+public class TipoVariable {
+	public static void main(String[] args) {
+	 double variable1 = 230.89;
+	 int variable1Entero = (int) variable1; //cast 
+	 
+	 System.out.println(variable1Entero);
+	 
+	 long prueba = 1222222222L;
+	  
+	 double resultado = (int)variable1 + variable1Entero;
+	 
+	 System.out.println(resultado);
+		
+		
+		
+	}
+}
