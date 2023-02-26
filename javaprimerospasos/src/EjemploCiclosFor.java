d4ffe971c2e55fb8981d925c8e47c7908dddefd6
Ximena Gallego <mxgallego@unicauca.edu.co>
2023-02-25 21:00:04 -0500

    sintaxis while y for

---
+++ javaprimerospasos/src/EjemploCiclosFor.java
@@ -0,0 +1,11 @@
+import java.util.Iterator;
+
+public class EjemploCiclosFor {
+	public static void main(String[] args) {
+		
+		
+		for(int contador=0; contador<=10; contador++) {
+			System.out.println(contador);	
+		}
+	}
+}
---
+++ javaprimerospasos/src/EjemplosCiclos.java
@@ -0,0 +1,13 @@
+
+public class EjemplosCiclos {
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		while (contador<=10) { //miengtras que (Condiccion)
+			//Ejecuta esto
+			System.out.println(contador);
+			contador++;
+			
+		}
+	}
+}
