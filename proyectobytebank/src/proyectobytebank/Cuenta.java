fb837393130220bc93668ee6d4a9b2837f3f6cb1
Ximena Gallego <mxgallego@unicauca.edu.co>
2023-02-27 19:25:01 -0500

    definir metodos con parametros y retorno

--- proyectobytebank/src/proyectobytebank/Cuenta.java
+++ proyectobytebank/src/proyectobytebank/Cuenta.java
@@ -8,7 +8,30 @@ public class Cuenta {
 	public  String titular;
 	
 	public void depositar(double valor){
-		saldo = saldo + valor;
+		//esta cuenta 
+		
+		this.saldo = this.saldo + valor;
+	}
+	
+	public boolean retirar(double valor) {
+		
+		if (this.saldo>=0) {
+			this.saldo = this.saldo - valor;
+			return true;
+		}else {
+			return false;
+		}		
+		
+	}
+	
+	public boolean transferir(double valor, Cuenta cuenta) {
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo  - valor;
+			cuenta.depositar(valor);
+			return true;
+		} 
+		return false;
+		
 	}
 	
 }
--- proyectobytebank/src/proyectobytebank/PruebasMetodos.java
+++ proyectobytebank/src/proyectobytebank/PruebasMetodos.java
@@ -6,5 +6,22 @@ public class PruebasMetodos {
 		miCuenta.saldo = 300;
 		miCuenta.depositar(200);
 		System.out.println(miCuenta.saldo);
+		
+		miCuenta.retirar(100);
+		System.out.println(miCuenta.saldo);
+		
+		Cuenta cuentaJimena = new Cuenta();
+		cuentaJimena.depositar(1000);
+		boolean puedotran = cuentaJimena.transferir(400, miCuenta);
+		
+		
+		if (puedotran) {
+			System.out.println("transferencia exitosa");
+		}
+		
+		System.out.println(cuentaJimena.saldo);
+		System.out.println(miCuenta.saldo);
+		
+		
 	}
 }
