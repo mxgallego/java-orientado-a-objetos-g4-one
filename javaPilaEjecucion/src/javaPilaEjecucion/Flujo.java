c889901f34af0b13f326901b31031acdce23c896
Ximena Gallego <mxgallego@unicauca.edu.co>
2023-03-02 22:05:13 -0500

    manejo de excepciones

--- javaPilaEjecucion/src/javaPilaEjecucion/Flujo.java
+++ javaPilaEjecucion/src/javaPilaEjecucion/Flujo.java
@@ -17,6 +17,21 @@ public class Flujo {
 		System.out.println("Inicio metodo2");
 		for (int i = 0; i < 5; i++) {
 			System.out.println(i);
+			
+			try {
+				if (i==3) {
+					int num = 0;
+					int resultado = i /num;
+					System.out.println(resultado);
+				}
+				
+				String test = null;
+				System.out.println(test.toString());
+			} catch (ArithmeticException  | NullPointerException exception) {
+				System.out.println("Atrapo Aritmetic");
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+			} 
 		}
 		System.out.println("fin metodo2");
 	}
